75bd36de24fc3efdde2dcdc4391a6fb3cbb782c4
nopezi <nopezisaputra.pratama@wingscorp.com>
2023-01-02 15:24:41 +0700

    push change key open api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-let env = require('../key.json')
+let env = require('../config/key.json')
 const modules = require('./modules/index.js')
 
 modules.whatsapp.controllers(env)
-// modules.whatsapp.models.gempa_terkini()
--- src/modules/whatsapp-bot/models/index.js
+++ src/modules/whatsapp-bot/models/index.js
@@ -1,10 +1,6 @@
-const gempa = require('./gempa.js')
-const db_gempa_terkini = require('./db-data-gempa-terkini.js')
 const db_bot = require('./bot-log.js')
 
 const models = {
-    gempa,
-    db_gempa_terkini,
     db_bot
 }
 
