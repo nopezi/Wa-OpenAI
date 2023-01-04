84560b7b42e85ff20dc500ecd2661f73cd33ca1c
nopezi <nopezisaputra.pratama@wingscorp.com>
2023-01-04 14:35:24 +0700

    push

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 let env = require('./config/key.json')
 const modules = require('./modules/index.js')
-const router = require('./routers/index.js')
 
 modules.whatsapp.controllers(env)
-
-// router()
