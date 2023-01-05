849d215fbbd270176622982f529763d223f13858
nopezi <nopezisaputra.pratama@wingscorp.com>
2023-01-05 11:09:10 +0700

    push heroku express

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ let env = require('./config/key.json')
 const modules = require('./modules/index.js')
 const router = require('./routers/index.js')
 
-modules.whatsapp.controllers(env)
+// modules.whatsapp.controllers(env)
 
 router()
